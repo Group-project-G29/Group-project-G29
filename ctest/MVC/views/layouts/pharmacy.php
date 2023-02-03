46d820c5fcf4033873b701745c1b2a3194484045
YNimantha <2020cs205@stu.ucsc.cmb.ac.lk>
2023-02-03 20:19:57 +0530

    minor changes 3

--- ctest/MVC/views/layouts/pharmacy.php
+++ ctest/MVC/views/layouts/pharmacy.php
@@ -8,11 +8,7 @@ include "../views/layouts/navbar/pharmacy-navbar.php";
 
 </nav>
     <div class="main-container">
-<<<<<<< HEAD
-         <?php $sidebar=new Sidebar(['Orders'=>'/ctest/pharmacist','Medicines'=>'/ctest/pharmacist','Advertisement'=>'#','Report'=>'#','My Detail'=>'#'],$select);?>
-=======
          <?php $sidebar=new Sidebar(['Orders'=>'/ctest/pharmacy-orders-pending','Medicines'=>'/ctest/pharmacy-view-medicine','Advertisement'=>'/ctest/pharmacy-view-advertisement','Report'=>'/ctest/pharmacy-view-report','My Detail'=>'/ctest/pharmacy-view-personal-details'],$select);?>
->>>>>>> 20000804
          <?php echo $sidebar;  ?>
         
         
@@ -24,11 +20,8 @@ include "../views/layouts/navbar/pharmacy-navbar.php";
                  </div>
                 <?php endif;?>
             </div>
-<<<<<<< HEAD
 
 
-=======
->>>>>>> 20000804
             {{content}}
         </div>
     </div>
