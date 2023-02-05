95fc71dc450d8fbe5d63ebb95b9e556dc290aa17
YNimantha <2020cs205@stu.ucsc.cmb.ac.lk>
2023-02-05 13:08:02 +0530

    receptionist controller fixed

--- ctest/MVC/views/layouts/pharmacy.php
+++ ctest/MVC/views/layouts/pharmacy.php
@@ -8,16 +8,10 @@ include "../views/layouts/navbar/pharmacy-navbar.php";
 
 </nav>
     <div class="main-container">
-<<<<<<< HEAD
-         <?php $sidebar=new Sidebar(['Orders'=>'/ctest/pharmacy-orders-pending','Medicines'=>'/ctest/pharmacy-view-medicine','Advertisement'=>'/ctest/pharmacy-view-advertisement','Report'=>'/ctest/pharmacy-view-report','My Detail'=>'/ctest/pharmacy-view-personal-details'],$select);?>
-         <?php echo $sidebar;  ?>
-        
-=======
          <?php $sidebar=new Sidebar(['Orders'=>'/ctest/pharmacist','Medicines'=>'/ctest/pharmacist','Advertisement'=>'#','Report'=>'#','My Detail'=>'/ctest/pharmacy-view-personal-details'],$select);?>
          <?php echo $sidebar;  ?>
         
         
->>>>>>> c9612a7f9b4ccb2af04ac81f9858298b29d36a1e
         <div class="sub-container">
             <div>
                 <?php if(Application::$app->session->getFlash('success')):?>
@@ -27,10 +21,6 @@ include "../views/layouts/navbar/pharmacy-navbar.php";
                 <?php endif;?>
             </div>
 
-<<<<<<< HEAD
-
-=======
->>>>>>> c9612a7f9b4ccb2af04ac81f9858298b29d36a1e
             {{content}}
         </div>
     </div>
