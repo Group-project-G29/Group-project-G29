2ef09d6ea8be1ce423749b4c51bbe1f5e34a12d7
YNimantha <2020cs205@stu.ucsc.cmb.ac.lk>
2023-02-09 18:38:35 +0530

    dfd

--- ctest/MVC/views/patient/pharmacy-main-page.php
+++ ctest/MVC/views/patient/pharmacy-main-page.php
@@ -31,49 +31,24 @@ use app\core\component\Component;
     </div>
     
     <section class="pharmacy-main-container">
-<<<<<<< HEAD
-        
-        <div class="search-medicine-container">
-            <?php echo $component->searchbar('',"name","search-bar--class1","Search by medicine name","search");?>
-            <?php if(Application::$app->session->get('user')):?>
-                <div class="main-cart-container">
-                    <?= $component->cartview() ?>
-                </div>
-            <?php endif; ?>
-            <script>
-                const searchbar=e('search');
-                const btn=e('bsearch');
-                btn.addEventListener("click",()=>{
-                    location.href="patient-pharmacy?cmd=search&value="+searchbar.value+"&page=1";
-                });
-            </script>
-        </div>
-=======
     <div class="ph-main-stripe">
         <img src="./media/images/patient/striper.png">
     </div>  
    
->>>>>>> 20002051
    
 
      <div class="ph-main-services">
         <div class="pharmacy-main-page--upload-prescription">
             <div class="service-content--1">
                 <h3>Upload Your precriptoin here</h3>
-<<<<<<< HEAD
-=======
                 <h4>We provide medicine to any valid prescription</h4>
->>>>>>> 20002051
                 <?= $component->button('upload-precription','',"Upload Prescription","button-class--yellow"); ?>
             </div>
         </div>
         <div class="pharmacy-main-page--track-orders"> 
             <div class="service-content--2">
                 <h3>Track you orders here</h3>
-<<<<<<< HEAD
-=======
                 <h4>Track yout order at comfort of your own home.</h4>
->>>>>>> 20002051
                 <?= $component->button('track-order','',"Track Your Order",'button-class--yellow','track-order'); ?>
             </div>
         </div>
@@ -132,14 +107,6 @@ function showSlides(){
 <script src="./media/js/main.js">
 
 </script>
-
-<script>
-    const trackOrderBtn=e('track-order');
-    trackOrderBtn.addEventListener('click',()=>{
-        location.href="patient-pharmacy?spec=order-main";
-    })
-</script>
-
 
 <script>
     const trackOrderBtn=e('track-order');
